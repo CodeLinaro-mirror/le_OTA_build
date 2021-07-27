eadf92b710623deb0d7cf95128400cd25109d118
Benergy Meenan Ravuri <bravuri@codeaurora.org>
2021-07-28 01:10:28 +0530

    Fix OTA zip generation failure
    correct the indentation for boot_img so that it will be
    assigned properly.
    
    Change-Id: Ibd27255429fc99407a790c778924ab7c31077037

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -813,8 +813,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
       symlinks = CopyPartitionFiles(system_items, input_zip, output_zip)
       script.MakeSymlinks(symlinks)
 
-    boot_img = common.GetBootableImage(
-        "boot.img", "boot.img", OPTIONS.input_tmp, "BOOT")
+  boot_img = common.GetBootableImage(
+      "boot.img", "boot.img", OPTIONS.input_tmp, "BOOT")
 
   if not block_based and not dm_verity_nand:
     def output_sink(fn, data):

595a18f59435b0fe6fc73dd7344d63971df87aae
Karim Dinani <quic_kdinani@quicinc.com>
2022-10-12 15:08:33 +0530

    Removes mirror changes from OTA.
    Change-Id: I57e524144f431e73a4b566ed646befffb940a357

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -403,14 +403,6 @@ class EdifyGenerator(object):
       self.script.append('unmount("%s");' % (p,))
     self.mounts = set()
 
-  def AddToZipMirror(self, input_zip, output_zip):
-    """Write updater-mirror-script which is used to copy active images
-    to inactive partitions  """
-
-    self.UnmountAll()
-    common.ZipWriteStr(output_zip, "META-INF/com/google/android/updater-mirror-script",
-                       "\n".join(self.script) + "\n")
-
   def AddToZip(self, input_zip, output_zip, input_path=None):
     """Write the accumulated script to the output_zip file.  input_zip
     is used as the source for the 'updater' binary needed to run

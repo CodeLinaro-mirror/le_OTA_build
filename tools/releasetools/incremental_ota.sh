d6cefc5b52e4158345c7c3cbedc815f4e3cc50d7
shrkum <quic_shrkum@quicinc.com>
2023-09-19 16:01:47 +0530

    OTA: Added support to get --mirror_sync as argument
    Pass --mirror_sync argument to ota script so that it can generate the
    updater-mirror-script which contains copy logic(from active to inactive
    after ota completes)
    
    Change-Id: Ie7e53623ac4be55c1d176e5f6792ed08cf2f8f8b

--- tools/releasetools/incremental_ota.sh
+++ tools/releasetools/incremental_ota.sh
@@ -55,6 +55,7 @@ export FSCONFIGFOPTS=" "
 block_based=" "
 python_version="python3"
 system_path=" "
+mirror_sync= ""
 
 if [ "$#" -gt 5 ]; then
     IFS=' ' read -a allopts <<< "$@"
@@ -66,6 +67,8 @@ if [ "$#" -gt 5 ]; then
            system_path="${allopts[${i}]}"
        elif [ "${allopts[${i}]}" = "--sign" ]; then
            sign_ota_package="${allopts[${i}]}"
+       elif [ "${allopts[${i}]}" = "--mirror_sync" ]; then
+           mirror_sync="${allopts[${i}]}"
        else
            FSCONFIGFOPTS=$FSCONFIGFOPTS${allopts[${i}]}" "
        fi
@@ -106,7 +109,7 @@ fi
 
 cd $target_files && zip -q $2 META/*filesystem_config.txt SYSTEM/build.prop && cd ..
 
-$python_version ./ota_from_target_files $block_based -n -v -d $device_type -v -p . -m linux_embedded --no_signing --system_mount_path $system_path -i $1 $2 update_incr_$4.zip > ota_debug.txt 2>&1
+$python_version ./ota_from_target_files $block_based  $mirror_sync -n -v -d $device_type -v -p . -m linux_embedded --no_signing --system_mount_path $system_path -i $1 $2 update_incr_$4.zip > ota_debug.txt 2>&1
 
 if [[ $? = 0 ]]; then
     echo "OTA zip signing started"

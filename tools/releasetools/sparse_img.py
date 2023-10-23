7d004457fd1915b96890355a40f8c8fdbbe6e3f9
P.Adarsh Reddy <quic_padarshr@quicinc.com>
2023-10-23 17:47:55 +0530

    Handle a corner case in system.map file parsing when creating ota zip.
    Account for the case where filename may also contain the ':' character,
    tokenize the lines in the map and extract the filename
    and blocks info correctly.
    
    Change-Id: I038b72adf251268b88453dee45d58d081187f1ee

--- tools/releasetools/sparse_img.py
+++ tools/releasetools/sparse_img.py
@@ -205,12 +205,12 @@ class SparseImage(object):
 
     with open(fn) as f:
       for line in f:
-        temp_list = line.split(':', 1)
-        if len(temp_list) == 1:
+        split_ind = line.rfind(':')
+        if split_ind == -1:
           # If no ':' are found, we are using an older version of make_ext4fs
           fn, ranges = line.split(None, 1)
         else:
-          fn, ranges = temp_list[0], temp_list[1]
+          fn, ranges = line[:split_ind], line[split_ind+1:]
         ranges = rangelib.RangeSet.parse(ranges)
         out[fn] = ranges
         assert ranges.size() == ranges.intersect(remaining).size()

889e093bb8e9b152692beaa07ec40809fa63215b
shrkum <quic_shrkum@quicinc.com>
2024-06-26 14:16:33 +0530

    OTA: Updated the name of the manifest.xml while zip generation
         Updates the name of common_manifest.xml
         to manifest.xml as per mplane specification
    
    Change-Id: I289af22cee9846de2757cafb855a3a415fda056e

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -3034,12 +3034,12 @@ def main(argv):
         merge_xmls(temp_xml_file1.name, temp_xml_file2.name, merged_xml_file.name)
         shutil.copyfile(merged_xml_file.name, temp_xml_file1.name)
       if(int(no_of_input_files) == 1):
-          common.ZipWriteStr(output_zip, "common_manifest.xml", manifest_xml_data)
+          common.ZipWriteStr(output_zip, "manifest.xml", manifest_xml_data)
       elif(x == int(no_of_input_files) - 1):
           try:
               with open(merged_xml_file.name) as fd:
                   merged_xml_file_data = fd.read()
-                  common.ZipWriteStr(output_zip, "common_manifest.xml", merged_xml_file_data)
+                  common.ZipWriteStr(output_zip, "manifest.xml", merged_xml_file_data)
           except IOError:
               print("Failed to open %s\n" % merged_xml_file.name)
           except Exception as e:
